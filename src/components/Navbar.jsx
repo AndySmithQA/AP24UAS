deaea72b3b03786c10653549ec38150b68dd8bc3
AndySmithQA <andy.smith@qa.com>
2024-07-24 15:15:46 +0100

    UI Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ export default function Navbar(){
             <a className="navbar-brand p-2" href="#">
             <img src={skoda} width="100"  alt="" />
         </a>
+        <b>AP24 UAS</b>
         <span id="title" className="lead fw-bold px-4 skoda-custom">Kodiaq Milage Tracking</span>
     </nav>
     
